e8110ca197c9015d991716dc50f4eeb1e88675b6
Ben Goldstein <bengoldstein9@gmail.com>
2023-05-01 19:38:19 -0700

    add ga

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
+// import {siteData}  from "@/data/site_data.json";
 
 export default function Document() {
+
   return (
     <Html lang="en">
-      <Head />
+
+
+      <Head>
+
+      </Head>
       <body>
         <Main />
         <NextScript />
