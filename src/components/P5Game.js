212470df964dc83d790ea6f464469f4f258e0984
Ben Goldstein <bengoldstein9@gmail.com>
2023-06-23 12:55:24 -0700

    p5 test

--- src/components/P5Game.js
+++ src/components/P5Game.js
@@ -1,77 +1,137 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const P5Game = (props) => {
-    const sketchRef = useRef();
-    const [p5, setP5] = useState(null);
-
-    useEffect(() => {
-        import('p5').then((p5) => {
-            setP5(p5);
-            let sketch = function(p) {
-                let shapes = [];
-                let blockColors = props.blockColors || ["red", "blue", "green", "yellow", "purple"];
-                let gravity = 0.5;
-
-                p.setup = function() {
-                    p.createCanvas(p.windowWidth, p.windowHeight);
-                }
-
-                p.draw = function() {
-                    p.background(220);
-                    shapes.forEach(shape => {
-                        p.fill(shape.color);
-                        shape.velocity += gravity;
-                        shape.y += shape.velocity;
-
-                        switch(shape.type) {
-                            case "rectangle":
-                                p.rect(shape.x, shape.y, 50, 50);
-                                break;
-                            case "circle":
-                                p.ellipse(shape.x, shape.y, 50, 50);
-                                break;
-                            case "triangle":
-                                p.triangle(shape.x, shape.y - 25, shape.x - 25, shape.y + 25, shape.x + 25, shape.y + 25);
-                                break;
-                            case "star":
-                                p.push();
-                                p.translate(shape.x, shape.y);
-                                p.beginShape();
-                                for(let i = 0; i < 5; i++) {
-                                    p.vertex(0, -20);
-                                    p.rotate(p.PI / 3);
-                                    p.vertex(0, -10);
-                                    p.rotate(p.PI / 3);
-                                }
-                                p.endShape(p.CLOSE);
-                                p.pop();
-                                break;
-                            default:
-                                break;
-                        }
-                    });
-                }
-
-                p.mousePressed = function() {
-                    let blockColor = blockColors[p.int(p.random(blockColors.length))];
-                    let shapeTypes = ["rectangle", "circle", "triangle", "star"];
-                    let shapeType = shapeTypes[p.int(p.random(shapeTypes.length))];
-
-                    shapes.push({
-                        x: p.mouseX,
-                        y: p.mouseY,
-                        color: blockColor,
-                        velocity: 0,
-                        type: shapeType
-                    });
-                }
-            };
-
-            new p5.default(sketch, sketchRef.current);
-        })
-    }, []);
-
-    return <div ref={sketchRef}></div>
+function loadScript(src, onLoad) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = onLoad;
+    document.body.appendChild(script);
 }
 
-export default P5Game;
+export default function P5Game() {
+    const [scriptsLoaded, setScriptsLoaded] = useState(false);
+    const gameRef = useRef();
+
+    useEffect(() => {
+        loadScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js', () => {
+            loadScript('https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.14.2/matter.min.js', () => {
+                setScriptsLoaded(true);
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        if (scriptsLoaded) {
+            const sketch = (p) => {
+                let Engine = window.Matter.Engine,
+                    World = window.Matter.World,
+                    Bodies = window.Matter.Bodies,
+                    Vertices = window.Matter.Vertices;
+
+                let engine;
+                let bodies = [];
+                let ground;
+                let score = 0;
+                let scoreP;
+                let canvas;
+
+                const shapes = ['rectangle',
+                    'square',
+                    // 'triangle',
+                    // 'circle', 'star'
+                ];
+
+                p.setup = function () {
+                    canvas = p.createCanvas(320, 320);
+                    engine = Engine.create();
+                    ground = Bodies.rectangle(160, 310, 320, 20, { isStatic: true });
+                    World.add(engine.world, [ground]);
+                    scoreP = p.createDiv().parent(gameRef.current);
+                };
+
+                p.draw = function () {
+                    p.background(51);
+                    window.Matter.Engine.update(engine);
+                    p.noStroke();
+                    bodies.forEach(body => {
+                        p.fill(body.color);
+                        p.push();
+                        p.translate(body.body.position.x, body.body.position.y);
+                        p.rotate(body.body.angle);
+                        switch (body.shape) {
+                            case 'rectangle':
+                            case 'square':
+                                p.rectMode(p.CENTER);
+                                p.rect(0, 0, body.w, body.h);
+                                break;
+                            case 'triangle':
+                                p.triangle(-body.size / 2, body.size / 2, body.size / 2, body.size / 2, 0, -body.size / 2);
+                                break;
+                            case 'circle':
+                                p.ellipseMode(p.RADIUS);
+                                p.ellipse(0, 0, body.size);
+                                break;
+                            case 'star':
+                                p.star(0, 0, body.size / 2, body.size, 5);
+                                break;
+                        }
+                        p.pop();
+                    });
+
+                    scoreP.html('Score: ' + score);
+                };
+
+                p.star = function (x, y, radius1, radius2, npoints) {
+                    let angle = p.TWO_PI / npoints;
+                    let halfAngle = angle / 2.0;
+                    p.beginShape();
+                    for (let a = 0; a < p.TWO_PI; a += angle) {
+                        let sx = x + p.cos(a) * radius2;
+                        let sy = y + p.sin(a) * radius2;
+                        p.vertex(sx, sy);
+                        sx = x + p.cos(a + halfAngle) * radius1;
+                        sy = y + p.sin(a + halfAngle) * radius1;
+                        p.vertex(sx, sy);
+                    }
+                    p.endShape(p.CLOSE);
+                };
+
+                p.mousePressed = function () {
+                    let shape = p.random(shapes);
+                    let size = Math.random() * 40 + 10;
+                    let body;
+                    if (shape === 'rectangle' || shape === 'square') {
+                        body = Bodies.rectangle(p.mouseX, p.mouseY, size, size);
+                    } else if (shape === 'circle') {
+                        body = Bodies.circle(p.mouseX, p.mouseY, size);
+                    } else if (shape === 'triangle') {
+                        let vertices = Vertices.fromPath('0 0 50 0 25 50');
+                        body = window.Matter.Bodies.fromVertices(p.mouseX, p.mouseY, vertices);
+                    } else if (shape === 'star') {
+                        let vertices = Vertices.fromPath('0 0 50 0 25 50');
+                        body = window.Matter.Bodies.fromVertices(p.mouseX, p.mouseY, vertices);
+                    }
+
+                    World.add(engine.world, [body]);
+                    let maxHeight = Math.min(...bodies.map(body => body.body.position.y));
+                    score = Math.round((320 - maxHeight) / 320 * 100);
+                    bodies.push({
+                        body,
+                        w: size,
+                        h: size,
+                        shape,
+                        color: p.color(Math.random() * 255, Math.random() * 255, Math.random() * 255),
+                        size
+                    });
+                };
+            };
+
+            new window.p5(sketch, gameRef.current);
+        }
+    }, [scriptsLoaded]);
+
+    return (
+        <div ref={gameRef}>
+            {!scriptsLoaded && <p>Loading...</p>}
+        </div>
+    );
+}
