cf18a4955b7e197f814c3ac243c26eef9ef70938
Ben Goldstein <bengoldstein9@gmail.com>
2023-09-11 18:09:32 -0700

    change to remove flash

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,62 +10,74 @@ import 'tailwindcss/tailwind.css'
 
 
 export const AppContext = createContext();
+// export default function MyApp({ Component, pageProps }) {
+//     const router = useRouter()
+//     const [theme, setTheme] = useState(siteData.theme);
+//     const [siteName, setSiteName] = useState(siteData.name);
+//     const [siteDesc, setSiteDesc] = useState(siteData.description);
+//     const [siteImgUrl, setSiteImgUrl] = useState(siteData.site_image);
+
+
+
+
+//     useEffect(() => {
+//         if (router.query.theme) {
+//             setTheme(router.query.theme)
+//         }
+//         if (router.query.siteName) {
+//             setSiteName(router.query.siteName);
+//         }
+//         if (router.query.siteDesc) {
+//             setSiteDesc(router.query.siteDesc);
+//         }
+//         if (router.query.siteImgUrl) {
+//             setSiteImgUrl(router.query.siteImgUrl);
+//         }
+//         const fetchSiteData = async () => {
+//             try {
+
+//                 const siteId = process.env.NEXT_PUBLIC_SITE_ID;
+//                 const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
+
+//                 const response = await fetch(`${baseUrl}/api/sites/${siteId}`);
+//                 // console.log(process.env)
+//                 // const response = await fetch(`${baseUrl}/api/sites/${siteId}/`); // Replace with your API endpoint
+//                 const data = await response.json();
+//                 setTheme(data.theme);
+//                 setSiteName(data.name);
+//                 setSiteDesc(data.description);
+//                 setSiteImgUrl(data.site_image);
+//             } catch (error) {
+//                 console.error('Error fetching site data:', error);
+//             }
+//         };
+
+//         fetchSiteData();
+//     }, [router.query])
+
+
+
+//     return (
+
+//         <ThemeProvider attribute="class">
+//             <AppContext.Provider value={{ siteName, theme, siteDesc, siteImgUrl }}>
+//                 <div data-theme={theme}>
+//                     <Component {...pageProps} />
+//                 </div>
+//             </AppContext.Provider>
+//         </ThemeProvider>
+
+//     )
+// }
+
 export default function MyApp({ Component, pageProps }) {
-    const router = useRouter()
-    const [theme, setTheme] = useState(siteData.theme);
-    const [siteName, setSiteName] = useState(siteData.name);
-    const [siteDesc, setSiteDesc] = useState(siteData.description);
-    const [siteImgUrl, setSiteImgUrl] = useState(siteData.site_image);
+  const theme = pageProps.site?.theme;
 
-
-
-
-    useEffect(() => {
-        if (router.query.theme) {
-            setTheme(router.query.theme)
-        }
-        if (router.query.siteName) {
-            setSiteName(router.query.siteName);
-        }
-        if (router.query.siteDesc) {
-            setSiteDesc(router.query.siteDesc);
-        }
-        if (router.query.siteImgUrl) {
-            setSiteImgUrl(router.query.siteImgUrl);
-        }
-        const fetchSiteData = async () => {
-            try {
-
-                const siteId = process.env.NEXT_PUBLIC_SITE_ID;
-                const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
-
-                const response = await fetch(`${baseUrl}/api/sites/${siteId}`);
-                // console.log(process.env)
-                // const response = await fetch(`${baseUrl}/api/sites/${siteId}/`); // Replace with your API endpoint
-                const data = await response.json();
-                setTheme(data.theme);
-                setSiteName(data.name);
-                setSiteDesc(data.description);
-                setSiteImgUrl(data.site_image);
-            } catch (error) {
-                console.error('Error fetching site data:', error);
-            }
-        };
-
-        fetchSiteData();
-    }, [router.query])
-
-
-
-    return (
-
-        <ThemeProvider attribute="class">
-            <AppContext.Provider value={{ siteName, theme, siteDesc, siteImgUrl }}>
-                <div data-theme={theme}>
-                    <Component {...pageProps} />
-                </div>
-            </AppContext.Provider>
-        </ThemeProvider>
-
-    )
+  return (
+    <ThemeProvider attribute="class">
+      <div data-theme={theme}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
