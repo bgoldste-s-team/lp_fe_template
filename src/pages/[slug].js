1f6a8f0ce25e092f01e6f32de1337800a1e93f97
Ben Goldstein <bengoldstein9@gmail.com>
2023-09-21 15:32:55 -0700

    add 404 to slug

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -39,48 +39,11 @@ export async function getStaticProps({ params }) {
 
   const data = site;
 
-  const fallBackPage = {
-            "id": 59303,
-            "user": 6,
-            "site": 221,
-            "created_at": "2023-08-26T08:11:48.854Z",
-            "updated_at": "2023-09-01T20:05:17.094Z",
-            "publish_date": "2023-08-26T15:11:00Z",
-            "is_homepage": false,
-            "show_in_navbar": true,
-            "page_group": "",
-            "page_group_slug": "",
-            "title": "Welcome to Pagesake!",
-            "page_description":"We're building your site and content right now, and everything should be ready shortly! You can refresh this page to see it update in realtime.",
-            "content_blocks": [
-                {
-                    "type": "TextBlock",
-                    "page": 0,
-                    "id": 0,
-                    "order": 0,
-                    "header": "Your Content Will Go Here",
-                    "subheader": "And it will be great",
-                    "body": "Our AI-powered content generation is hard at work building your site right now. Refresh this page to see it update, or head back to your dashboard to make changes to your site",
-                    "caption": null,
-                    "cta1_text": "Dashboard",
-                    "cta1_link": "https://pagesake.com/dashboard",
-                    "cta2_text": "Help",
-                    "cta2_link": "https://pagesake.com/help",
-                    "image_url": ""
-                },
-               
-            ],
-            "image_url": null,
-            "is_new": false,
-            "has_all_blocks": true,
-            "slug": ""
-        }
   const page = data.pages.find((p) => p.slug === slug) ;
   console.log(page)
 
 
 
-  const returnPage = page ? page : fallBackPage;
 
   if(!page) {
     console.log("PAGE NTO FOUND REDIRECT")
@@ -93,7 +56,7 @@ export async function getStaticProps({ params }) {
   }
   return {
     props: {
-        returnPage ,
+        page ,
         site
     },
       revalidate: 10, // In seconds
