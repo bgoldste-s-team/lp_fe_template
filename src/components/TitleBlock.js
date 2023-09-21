6400324620b32276c1f5937ee1194f4bcc4df3a6
Ben Goldstein <bengoldstein9@gmail.com>
2023-09-21 15:11:34 -0700

    update text title to avoid breaks

--- src/components/TitleBlock.js
+++ src/components/TitleBlock.js
@@ -5,10 +5,10 @@ const TitleBlock = ({page}) => {
 		<div className="hero min-h-[60vh] bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary to-secondary" style={{backgroundImage: `${page.image_url ? `url(${page.image_url})`: ''}`}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold ">{page.title}</h1>
-      <p className="mb-5">{page.page_description}</p>
-      <p className="">Last Updated: {new Date(page.publish_date).toLocaleDateString()}</p>
+    <div className="max-w-2xl">
+      <h1 className="py-6  text-2xl sm:text-4xl md:text-5xl font-bold break-words ">{page.title}</h1>
+      <p className="py-2">{page.page_description}</p>
+      <p className="text-sm py-2">Last Updated: {new Date(page.publish_date).toLocaleDateString()}</p>
    
      {/* <button className="btn btn-primary">Get Started</button>
       <button className="btn btn-primary">Get Started</button>*/}
