b26c23b4dbd79e73b10405a619f605490b120244
Andy Wang <andywangpt@gmail.com>
2023-11-27 13:20:03 -0800

    navbar formatted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,53 +3,48 @@ import Link from "next/link";
 const Navbar = ({ site }) => {
   const siteName = site.name;
   const pages = site.pages;
-  const determineFontSize = (text) => {
-    const length = text.length;
-
-    if (length < 10) return "text-2xl leading-snug";
-    if (length < 20) return "text-xl leading-snug";
-    if (length < 30) return "text-base leading-snug";
-    return "text-md leading-snug";
-  };
-  const dynamicFontSize = determineFontSize(siteName);
 
   return (
-    <div className="navbar bg-base-100 z-50 top-0 sticky flex items-center">
-      <div className="navbar-start sm:pb-0">
-        <div className=" break-words py-4 ">
+    <div className="navbar bg-neutral">
+      <div className="navbar-start w-full">
+        <div className="">
           <Link
             href="/"
-            className={`text-left btn btn-ghost normal-case   ${dynamicFontSize}`}
+            className={`text-left normal-case text-lg sm:text:2xl md:text-3xl  text-primary-content`}
           >
             {siteName}
           </Link>
         </div>
       </div>
+
       <div className="navbar-end">
         <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-primary btn-outline">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-base-content"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+            <label tabIndex={0} className="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-neutral-content"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+            </label>
+                  
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
+            
             <li key={"home"}>
               <Link href="/">Home</Link>
             </li>
+
             {pages
               .filter((p) => p.show_in_navbar === true && !p.is_homepage)
               .map((p) => (
@@ -57,6 +52,7 @@ const Navbar = ({ site }) => {
                   <Link href={p.slug}>{p.title}</Link>
                 </li>
               ))}
+
             <li key={"search"}>
               <Link href="/search">Search</Link>
             </li>
